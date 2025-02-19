d9bfe0b02611c47e0fd37d7ede85657ac36dc5f9
yoon-zh <yoon-zh@users.noreply.github.com>
2025-02-19 11:35:38 +0000

    deploy: 0c966f195943cbebc86bd0d3f4b1e4d20739763d

---
+++ scripts/axeptio.js
@@ -0,0 +1,19 @@
+window.axeptioSettings = {
+  clientId: "67b43932f7dcf197504f46c7",
+  cookiesVersion: "yoonzh-en-EU",
+  googleConsentMode: {
+    default: {
+      analytics_storage: "denied",
+      ad_storage: "denied",
+      ad_user_data: "denied",
+      ad_personalization: "denied",
+      wait_for_update: 500
+    }
+  }
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
